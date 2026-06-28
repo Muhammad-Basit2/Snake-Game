ea528e09cd2b60d2010558f1fcc1135c229a9c95
Muhammad-Basit2 <muhammadbasit9274@gmail.com>
2026-06-29 00:12:00 +0500

    try to fix error

--- script.js
+++ script.js
@@ -49,13 +49,13 @@ setInterval(() => {
   let head = null;
 
   if (direction === "left") {
-    head = { x: snake[ 0 ].x, y: snake[ 0 ].y - 1 };
+    head = { x: snake[0].x, y: snake[0].y - 1 };
   } else if (direction === "right") {
-    head = { x: snake[ 0 ].x, y: snake[ 0 ].y + 1 };
+    head = { x: snake[0].x, y: snake[0].y + 1 };
   } else if (direction === "down") {
-    head = { x: snake[ 0 ].x + 1, y: snake[ 0 ].y };
+    head = { x: snake[0].x + 1, y: snake[0].y };
   } else if (direction === "up") {
-    head = { x: snake[ 0 ].x - 1, y: snake[ 0 ].y };
+    head = { x: snake[0].x - 1, y: snake[0].y };
   }
 
   snake.forEach((segment) => {
@@ -68,6 +68,6 @@ setInterval(() => {
   render();
 }, 400);
 
-addEventListener("keydown",(event)=>{
+addEventListener("keydown", (event) => {
   console.log(event.key)
 })
